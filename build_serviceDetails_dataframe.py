eab263e29b08fb6cda8d3a7afa853efff9d88d27
kenneth.g.mackinnon <kenneth.g.mackinnon@gmail.com>
2018-04-25 12:50:50 -0400

    write out csv file

--- build_serviceDetails_dataframe.py
+++ build_serviceDetails_dataframe.py
@@ -1,14 +1,30 @@
 import matplotlib
 matplotlib.use('Agg')
-
+import os
 import json
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
+  from_loc = 'PMT'
+  to_loc = 'GLQ'
+  from_time = '0756'
+  to_time = '0915'
+  from_date = '2018-01-27'
+  to_date = '2018-03-26'
+  days = 'WEEKDAY'
+  service_file = ''.join((from_loc,
+                 to_loc,
+                 from_time,
+                 to_time,
+                 from_date,
+                 to_date,
+                 days,
+                 ".json"))
+  qualified_file = os.path.join('data', 'aggregated', service_file)
   full_df = pd.DataFrame([])
-  with open('serviceDetails.json', 'r') as infile:
+  with open(qualified_file, 'r') as infile:
     for in_line in infile:
       #need to use loads rather than load - might be because of \n?
       serviceDetails = json.loads(in_line)
@@ -31,17 +47,19 @@ def main():
   full_df['td_diff'] = (full_df['actual_td'] - full_df['gbtt_ptd']).astype('timedelta64[m]')
   full_df['actual_lay'] = (full_df['actual_td'] - full_df['actual_ta']).astype('timedelta64[m]')
   full_df['gbtt_lay'] = (full_df['gbtt_ptd'] - full_df['gbtt_pta']).astype('timedelta64[m]')
+  # write out as csv for future use
+  full_df.to_csv("train_times.csv")
   # print and describe resulting df
   #print(full_df.describe())
   #print(full_df.head())
   # create pivot of arrival times by station
   pvt = full_df.pivot_table('ta_diff',rows='rid', cols='location')
-  pvt = pvt.drop(['GLQ'], axis=1)
+  pvt = pvt.drop(['EDB'], axis=1)
 
   print(pvt)
   plt.figure()
   pvt.hist(stacked=True)
-  plt.savefig('hist.png')
+  plt.savefig('hist2.png')
 
 
 if __name__ == '__main__':
